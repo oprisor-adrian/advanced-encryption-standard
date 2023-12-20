7fe0f788954fcd5348efd5c2fb10a494c812ceba
Oprișor Adrian-Ilie <88934968+oprisor-adrian@users.noreply.github.com>
2023-12-20 23:54:26 +0200

    Add AES encryption

---
+++ include/aes.h
@@ -0,0 +1,29 @@
+#ifndef BLOCK_CIPHER_AES_H_
+#define BLOCK_CIPHER_AES_H_
+
+#include "../external/byte_utils/include/byte_vector.h"
+#include "../external/byte_utils/include/word.h"
+
+namespace BlockCipher {
+
+class AES {
+  public:
+    AES(const ByteUtils::ByteVector& key);
+    const ByteUtils::ByteVector& Encrypt(const ByteUtils::ByteVector& plain);
+  private:
+    void KeyExpansion();
+    ByteUtils::Word SubWord(const ByteUtils::Word word);
+    ByteUtils::Word RotWord(const ByteUtils::Word word);
+    void AddRoundKey(const std::size_t round);
+    void SubByte();
+    void ShiftCols();
+    void MixColumns();
+    ByteUtils::ByteVector key_;
+    ByteUtils::ByteVector state_;
+    std::size_t key_wsize_;
+    std::size_t rounds_;
+};
+
+}  // namespace BlockCipher
+
+#endif  // BLOCK_CIPHER_AES_H_
--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 #include <string>
 #include <vector>
 
-#include "include/aes_256.h"
+#include "include/aes.h"
 
 int main() {
   std::string input = "00112233445566778899aabbccddeeff";
   std::string key = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f";
-  BlockCipher::AES256 aes256({input}, {key});
-  std::cout << aes256.Encrypt() << std::endl;
+  BlockCipher::AES aes(key);
+  std::cout << aes.Encrypt(input).ToHex() << std::endl;
   return 0;
 }
