027595bca78436a81840aff36a93b8044ca1fb8a
Oprișor Adrian-Ilie <88934968+oprisor-adrian@users.noreply.github.com>
2023-12-20 23:47:31 +0200

    Add ByteVector class

--- external/byte_utils/include/byte_vector.h
+++ external/byte_utils/include/byte_vector.h
@@ -1,6 +1,7 @@
 #ifndef BYTE_UTILS_BYTE_VECTOR_H_
 #define BYTE_UTILS_BYTE_VECTOR_H_
 
+#include <ostream>
 #include <string>
 #include <vector>
 
@@ -8,10 +9,28 @@
 
 namespace ByteUtils {
 
+class Word;
+
 class ByteVector{
   public:
+    ByteVector() = default;
     ByteVector(const std::string& hex_string);
-    ByteVector(const std::vector<Byte> byte_vector);
+    ByteVector(const std::vector<Byte> bytes);
+    friend std::ostream& operator<<(std::ostream& stream, 
+                                    const ByteVector& bytes);
+    // Returns the `Byte` from the position `pos`.
+    const Byte operator[](const std::size_t pos) const;
+    // Acces the `Byte` from the position `pos`.
+    Byte& operator[](const std::size_t pos);
+    // Pushs back the bytes from the `Word` object.
+    void PushBack(const Word word);
+    // Returns the `Word` from the position `pos`.
+    const Word GetWord(const std::size_t pos) const;
+    // Returns `count` words from vector.
+    const std::vector<Word> GetWord(const std::size_t pos, 
+                                    const std::size_t count);
+    const std::string ToHex() const;
+    inline const std::size_t GetSize() const { return bytes_.size(); }
   private:
     std::vector<Byte> bytes_;
 };
