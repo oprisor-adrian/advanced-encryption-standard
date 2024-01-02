95e1f96ac232c11535dde08e04e4b4eb6eb99907
oprisor-adrian <oprisoradrianilie@gmail.com>
2024-01-02 15:55:40 +0000

    Test descrypt method

--- test/test_aes.cpp
+++ test/test_aes.cpp
@@ -23,30 +23,44 @@
 
 #include "aes.h"
 
-struct TestCase {
-  std::string key;
-  std::string plain_text;
-  std::string cipher_text;
+class AesTest: public ::testing::Test {
+  protected:
+    struct TestCase {
+      std::string key;
+      std::string plain_text;
+      std::string cipher_text;
+    };
+    void SetUp() override {
+      test_cases = {
+        {"000102030405060708090a0b0c0d0e0f", 
+         "00112233445566778899aabbccddeeff", 
+         "69c4e0d86a7b0430d8cdb78070b4c55a"
+        },
+        {"000102030405060708090a0b0c0d0e0f1011121314151617",
+         "00112233445566778899aabbccddeeff",
+         "dda97ca4864cdfe06eaf70a0ec0d7191"
+        },
+        {"000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
+         "00112233445566778899aabbccddeeff",
+         "8ea2b7ca516745bfeafc49904b496089"
+        }
+      };
+    }
+    std::vector<TestCase> test_cases;
 };
 
-TEST(TestAes, TestEncrypt) {
-  std::vector<TestCase> test_cases = {
-    {"000102030405060708090a0b0c0d0e0f", 
-     "00112233445566778899aabbccddeeff", 
-     "69c4e0d86a7b0430d8cdb78070b4c55a"
-    },
-    {"000102030405060708090a0b0c0d0e0f1011121314151617",
-     "00112233445566778899aabbccddeeff",
-     "dda97ca4864cdfe06eaf70a0ec0d7191"
-    },
-    {"000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
-     "00112233445566778899aabbccddeeff",
-     "8ea2b7ca516745bfeafc49904b496089"
-    }
-  };
+TEST_F(AesTest, TestEncrypt) {
   Cryptography::AES aes;
   for (const auto& test : test_cases) {
     std::string output = aes.Encrypt(test.plain_text, test.key).ToHex();
-    ASSERT_STREQ(output.c_str(), test.cipher_text.c_str());
+    EXPECT_STREQ(output.c_str(), test.cipher_text.c_str());
+  }
+}
+
+TEST_F(AesTest, TestDecrypt) {
+  Cryptography::AES aes;
+  for (const auto& test : test_cases) {
+    std::string output = aes.Decrypt(test.cipher_text, test.key).ToHex();
+    EXPECT_STREQ(output.c_str(), test.plain_text.c_str());
   }
 }
